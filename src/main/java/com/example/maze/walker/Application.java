72d62be9407ec4ca29c10be275cd634af24f6f29
root <root@Budapesht.localdomain>
2022-10-26 00:57:42 +0600

    Clean up

--- src/main/java/com/example/maze/walker/Application.java
+++ src/main/java/com/example/maze/walker/Application.java
@@ -5,9 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Application {
     public static void main(String[] args) throws Exception {
-        JsonNode nodeA = new ObjectMapper()
-                .readTree("{ \"a\": 1 }")
-                .get("a");
-        System.out.println(nodeA.toString());
+
     }
 }
